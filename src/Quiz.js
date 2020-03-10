b6ccd37b15821bb4a93966ec1f6b53d1c785e274
KISHAN L <kishan.karthi7@gmail.com>
2020-03-10 16:59:41 +0100

    handling button click events

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,13 @@ class Quiz extends Component {
     super(props);
     this.state = { quiz_position: 1 };
   }
+
+  showNextQuestion() {
+    this.setState(state => {
+      return { quiz_position: state.quiz_position + 1 };
+    });
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -18,6 +25,7 @@ class Quiz extends Component {
           <QuizEnd />
         ) : (
           <QuizQuestion
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
